5533a18b2dec74c1bbe13a61eea574b0525560ef
Henry <henrywl@gmail.com>
2024-05-14 19:18:01 +0100

    basic getting chat messages and users added

--- src/components/chat/ChatMessages.jsx
+++ src/components/chat/ChatMessages.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatMessage from './ChatMessage'
 
 function ChatMessages() {
+    const [isLoading, setIsLoading] = useState(true)
+    const [chatData, setChatData] = useState([])
+    // using route get chat id from url then load messages for specific chat
+    useEffect(() => {
+        const fetchChatData = async() => {
+            const response = await fetch('http://localhost:3000/api/chats/663ea7f2f886bd698e9114f7')
+            const data = await response.json()
+
+            console.log(data)
+            setIsLoading(false)
+            setChatData(data.chat)
+        }
+
+        fetchChatData()
+    }, [])
+
   return (
-    <div className='border-green-100 border-8 w-3/4'>Chat main box
-       <ChatMessage/>
+    <div className='border-green-100 border-8 w-3/4 overflow-y-scroll'>Chat main box
+       {/* <ChatMessage/> map and user chat message pass single chat data as props in */}
+       {isLoading && <p>Loading</p>}
+
+       {!isLoading && chatData && chatData.messages.map((chat) => {
+        return (
+            <div className='border-pink-500 border m-2'>
+                <p>{chat.sender}</p>
+                <p>{chat.text}</p>
+                <p>{chat.time}</p>
+
+            </div>
+        )
+       })}
     </div>
   )
 }
--- src/components/chat/ChatUsers.jsx
+++ src/components/chat/ChatUsers.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function ChatUsers() {
+  const [chatUsers, setChatUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3000/api/chats");
+      const data = await response.json();
+      console.log(data);
+      setChatUsers(data.chats);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div className='border-red-100 border-8 w-1/4'>Users on left</div>
-  )
+    <div className="border-red-100 border-8 w-1/4">
+      Users on left
+      <div>{isLoading && <p>Loading...</p>}</div>
+
+      {!isLoading && chatUsers && <div>{chatUsers.map((chat) => <p>{chat._id}</p>)}</div>}
+    </div>
+  );
 }
 
-export default ChatUsers
+export default ChatUsers;
