c253e2da3eec6d239a30b615163a69389bd2fee8
Henry <henrywl@gmail.com>
2024-05-15 22:44:00 +0100

    chat function working with chat bubbles

--- src/components/chat/ChatMessages.jsx
+++ src/components/chat/ChatMessages.jsx
@@ -1,49 +1,92 @@
-import React, { useEffect, useState } from 'react'
-import ChatMessage from './ChatMessage'
-import ChatInput from './ChatInput'
+import React, { useContext, useEffect, useState } from "react";
+import ChatMessage from "./ChatMessage";
+import ChatInput from "./ChatInput";
+import authContext from "../../context/auth-context";
 
 function ChatMessages(props) {
-    const [isLoading, setIsLoading] = useState(true)
-    const [chatData, setChatData] = useState([])
-    const [newMessageSent, setNewMessageSent] = useState('')
+  const [isLoading, setIsLoading] = useState(true);
+  const [chatData, setChatData] = useState([]);
+  const [newMessageSent, setNewMessageSent] = useState("");
 
-    const newMessageSentHandler = (textInput) => {
-        setNewMessageSent(textInput)
-    }
+  const auth = useContext(authContext);
 
+  console.log(auth, "AUTH");
 
-    // using route get chat id from url then load messages for specific chat
-    useEffect(() => {
-        const fetchChatData = async() => {
-            const response = await fetch(`http://localhost:3000/api/chats/${props.chatId}`)
-            const data = await response.json()
+  const newMessageSentHandler = (textInput) => {
+    setNewMessageSent(textInput);
+  };
 
-            console.log(data)
-            setIsLoading(false)
-            setChatData(data.chat)
-        }
+  // using route get chat id from url then load messages for specific chat
+  useEffect(() => {
+    const fetchChatData = async () => {
+      const response = await fetch(
+        `http://localhost:3000/api/chats/${props.chatId}`
+      );
+      const data = await response.json();
 
-        fetchChatData()
-    }, [props.chatId, newMessageSent])
+      console.log(data);
+      setIsLoading(false);
+      setChatData(data.chat);
+    };
+
+    fetchChatData();
+  }, [props.chatId, newMessageSent]);
 
   return (
-    <div className='border-green-100 border-8 w-3/4 overflow-y-scroll flex flex-col'>
-    <div className='flex-1 relative'>
-      {/* Chat messages */}
-      {isLoading && <p>Loading</p>}
-      {!isLoading && chatData && chatData.messages.map((chat) => (
-        <div className='border-pink-500 border m-2' key={chat._id}>
-          <p>{chat.sender}</p>
-          <p>{chat.text}</p>
-          <p>{chat.time}</p>
-        </div>
-      ))}
+    <div className="border-green-100 border-8 w-3/4 overflow-y-scroll flex flex-col">
+      <div className="flex-1 relative">
+        {/* Chat messages */}
+        {isLoading && <p>Loading</p>}
+        {!isLoading &&
+          chatData &&
+          chatData.messages.map((chat) => {
+            const date = new Date(chat.time);
+            const timeString = `${("0" + date.getUTCHours()).slice(-2)}:${(
+              "0" + date.getUTCMinutes()
+            ).slice(-2)}`;
+            return (
+              <div
+                className={
+                  chat.sender === auth.userId
+                    ? "chat chat-end"
+                    : "chat chat-start"
+                }
+              >
+                <div className="chat-image avatar">
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="Tailwind CSS chat bubble component"
+                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    />
+                  </div>
+                </div>
+                <div className="chat-header">
+                  {chat.sender}
+                  {/* get chatters name */}
+                  <time className="text-xs opacity-50">{timeString}</time>
+                </div>
+                <div
+                  className={
+                    chat.sender === auth.userId
+                      ? "chat-bubble chat-bubble-success"
+                      : "chat-bubble chat-bubble-primary"
+                  }
+                >
+                  {chat.text}
+                </div>
+                <div className="chat-footer opacity-50">Delivered</div>
+              </div>
+            );
+          })}
+      </div>
+      <div className="bg-white p-4 border-blue-500 border-2">
+        <ChatInput
+          chatId={props.chatId}
+          newMessageSentHandler={newMessageSentHandler}
+        />
+      </div>
     </div>
-    <div className='bg-white p-4 border-blue-500 border-2'>
-      <ChatInput chatId={props.chatId} newMessageSentHandler={newMessageSentHandler}/>
-    </div>
-  </div>
-  )
+  );
 }
 
-export default ChatMessages
+export default ChatMessages;
